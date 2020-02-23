c141dd9a4e0c98b460f28b1799cea223ac54fd90
Jose Dominguez <41589772+jose-dom@users.noreply.github.com>
2020-02-23 15:18:57 -0500

    Add files via upload

--- alpha/middleend/index.php
+++ alpha/middleend/index.php
@@ -1,40 +1,63 @@
+<h1>CS 490</h1>
+<h2>Middle End</h2>
 <?php
+//grabbing json from backend
 $str_json = file_get_contents('php://input'); 
-$response = json_decode($str_json, true); // decoding received JSON to array
-$ucid="none";$pass="none";
+//decoding json into response array
+$response = json_decode($str_json, true); 
+//initial setting of variables
+$ucid="none";
+$pass="none";
+
 if(isset($response['ucid'])) $ucid = $response['ucid'];
 if(isset($response['pass'])) $pass = $response['pass'];
 
-$res_proejct=login_project($ucid,md5($pass));	
+$res_project=login_project($ucid,md5($pass));	
 $res_njit=login_njit($ucid,$pass);
-print "<center><h2>".$res_proejct.' '.$res_njit."</h2></center>";
+$data = array($res_project,$res_njit);
+echo json_encode($data);
+
 
 // curl backend 
 function login_project($ucid,$pass){
+	//data from json response
 	$data = array('ucid' => $ucid,'pass' =>$pass);
+	//url to backend
 	$url = "https://web.njit.edu/~pk549/490/alpha/login.php";
+	//initialize curl session and return a curl handle
 	$ch = curl_init();
+	//options for a curl transfer
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+	//execute curl session
 	$response = curl_exec($ch);
+	//close curl session
 	curl_close ($ch);
-	return $response;
+	//decoding response
+	$response_decode = json_decode($response);
+	//return response
+	return $response_decode[0];
 }
 
 
 // curl njit
 function login_njit($ucid,$pass){
-	$url = "http://myhub.njit.edu/vrs/";
-	$data= array("user" => $ucid,"pass" =>$pass,"uuid" => "0xACA021");
+	//url to njit 
+	$url = "http://myhub.njit.edu/vrs/ldapAuthenticateServlet";
+	//data from json response
+	$data= array("user_name" => 'jrd62',"passwd" =>'JrDom1997');
+	//initialize curl session and return a curl handle
 	$ch = curl_init();
+	//options for a curl transer
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data)); 
+	//execute curl session
 	$response = curl_exec($ch);
+	//close curl session
 	curl_close ($ch);
-
-	if (strpos($response,"Rejected")==false) return "NJIT Accept";
-	return "NJIT Reject";
+	//return response
+	return $response;
 }
 ?>
