2941c71343ff2f26fc33806a60e5d699dbc4d930
Paul Kim <pkim168@gmail.com>
2020-03-24 03:20:05 -0400

    Update teacherExamList.php

--- beta/frontend/teacherExamList.php
+++ beta/frontend/teacherExamList.php
@@ -66,7 +66,7 @@
 				$data = array();
 				$data['requestType'] = 'getExams';
 				$data['ucid'] = $_SESSION['ucid'];
-				$url = "https://web.njit.edu/~pk549/490/beta/examTbl.php";
+				$url = "https://web.njit.edu/~jrd62/CS490/beta/teacher_middle_exam.php";
 				$ch = curl_init($url);
 				$payload = json_encode($data);
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
