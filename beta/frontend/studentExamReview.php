df9ff5961ad4d4a80f9fe4c2cd8e8906dfc47ac2
dpn418 <60945105+dpn418@users.noreply.github.com>
2020-03-24 07:10:05 -0400

    fas

--- beta/frontend/studentExamReview.php
+++ beta/frontend/studentExamReview.php
@@ -6,15 +6,16 @@
         'cookie_secure' => 1,
         'cookie_httponly' => 1
     ]);
-	ob_start();
 	if (empty($_SESSION['ucid']) || empty($_SESSION['role'])){
 		header('Location: ./index.php');
 	} 
-	if ($_SESSION['role'] == '2') {
-		header('Location: ./teacherView.php');
+	if ($_SESSION['role'] != '2') {
+		header('Location: ./index.php');
 	}
+	
 	$data = array();
-	$data['requestType'] = 'getStudentExams';
+	$data['requestType'] = 'getStudentAnswers';
+	$data['examId'] = $_SESSION['examId'];
 	$data['ucid'] = $_SESSION['ucid'];
 	$url = "https://web.njit.edu/~jrd62/CS490/beta/student_middle.php";
 	
@@ -26,20 +27,16 @@
 	$result = curl_exec($ch);
 	curl_close($ch);
 	$json = json_decode($result, true);
+	
+	
+	
+	ob_start();
 ?>
 <html>
 	<head>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="styles.css">
 		<script>
-			function takeExam(id) {
-				location.href = "".concat('https://web.njit.edu/~dn236/CS490/beta/ExamPage.php?examId=', id);
-			}
-			
-			function viewResults(id) {
-				$_SESSION['examId'] = id;
-				location.href = "".concat('https://web.njit.edu/~dn236/CS490/beta/studentExamReview.php?examId=', id);
-			}
 		</script>
 	</head>
 	<body>
@@ -48,27 +45,14 @@
 		?>
 		<div class="flex-container column" style="width: 100%; margin: 0%; float:left; border-right: 1px black solid;">
 			<div class="flex-container row">
-				<h1> Exams </h1>
+				<h1> Exam <?php echo $_SESSION['examId']?> </h1>
 			</div>
 			<div class="flex-container row" style="width:98%; float:left">
 				<table id="exams" style="width:100%">
-					<tr>
-						<th> Exam Id </th>
-						<th> Status </th>
-					</tr>
 					<?php
 						for ($i = 0; $i < count($json); $i++) {
 							echo "<tr>";
-							echo "<td>".$json[$i]['examId']."</td>";
-							if ($json[$i]['status'] == 0) {
-								echo "<td>Not Taken</td>";
-								echo "<td><button type='button' id='".$json[$i]['examId']."' style='height: 40px; width: 100%' onclick='takeExam(this.id)'>Take Exam</button></td>";
-							} else if ($json[$i]['status'] == 1) {
-								echo "<td>Graded</td>";
-							} else {
-								echo "<td>Released</td>";
-								echo "<td><button type='button' id='".$json[$i]['examId']."' style='height: 40px; width: 100%' onclick='viewResults(this.id)'>View Results</button></td>";
-							}
+							echo "
 							echo "</tr>";
 						}
 					?>
@@ -78,4 +62,4 @@
 		
 	</body>
 </html>
-<?php ob_flush();?>
+<?php ob_flush();?>	
