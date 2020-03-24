8ce9495027cc15dc04bc27995c2ef149836df91c
Paul Kim <pkim168@gmail.com>
2020-03-23 21:33:28 -0400

    Update teacherView.php

--- beta/frontend/teacherView.php
+++ beta/frontend/teacherView.php
@@ -7,12 +7,12 @@
         'cookie_httponly' => 1
     ]);
 	
-	if (empty($_SESSION['ucid']) || empty($_SESSION['role'])){
+	/* if (empty($_SESSION['ucid']) || empty($_SESSION['role'])){
 		header('Location: ./index.php');
 	} 
 	if ($_SESSION['role'] != '2') {
 		header('Location: ./index.php');
-	}
+	} */
 	ob_start();
 	
 	
