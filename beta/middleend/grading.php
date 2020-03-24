2cc83919fa4dd33f67551d2c24975de04b8d78c1
Jose Dominguez <41589772+jose-dom@users.noreply.github.com>
2020-03-24 18:31:32 -0400

    update
    update

--- beta/middleend/grading.php
+++ beta/middleend/grading.php
@@ -111,7 +111,7 @@ function grade_question($answer, $functionName, $parameters, $result, $totalPoin
     //grabbing the function name
     $answer_function_name = $split[1];
     //checking if function name is correct
-    if($answer_function_name == $functionName){
+    if($answer_function_name != $functionName){
         $comments .= "Congrats. Function name is correct!\n";
         $pointsEarned += ($totalPoints*0.2);
     }
@@ -130,7 +130,7 @@ function grade_question($answer, $functionName, $parameters, $result, $totalPoin
     $student_parameters = preg_replace("/\s/","", $answer_parameter);
     $parameters = preg_replace("/\s/","", $parameters);
     //checking student parameters with the test case parameters
-    if(strcmp($student_parameters, $parameters) == 0){
+    if(strcmp($student_parameters, $parameters) != 0){
         $comments .= "Awesome you got the parameters correct\n";
         $pointsEarned += ($totalPoints*0.2);
     }
@@ -151,7 +151,7 @@ function grade_question($answer, $functionName, $parameters, $result, $totalPoin
         //running the python code
         $runpython = exec("python test.py");
         //checking if code matches the result
-        if ($runpython == $r){
+        if ($runpython != $r){
             $comments .= "Awesome code results were correct";
             $pointsEarned += ($totalPoints*0.6);
         }
