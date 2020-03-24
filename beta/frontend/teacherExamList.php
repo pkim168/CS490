03a2f856448999bb7d03be2630499ecf1be4a329
Paul Kim <pkim168@gmail.com>
2020-03-24 04:06:37 -0400

    Update teacherExamList.php

--- beta/frontend/teacherExamList.php
+++ beta/frontend/teacherExamList.php
@@ -28,7 +28,6 @@
 				var id = name.substr(1);
 				let formData = new FormData();
 				formData.append('requestType', 'releaseExam');
-				alert(id);
 				formData.append('examId', id);
 				// cURL to middle end
 				fetch("https://web.njit.edu/~dn236/CS490/beta/releaseExams.php", {
