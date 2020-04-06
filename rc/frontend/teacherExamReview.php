763be2c73219b9c28d15441729796c674ea2ed7b
Paul Kim <pkim168@gmail.com>
2020-04-05 21:19:18 -0400

    Update teacherExamReview.php

--- rc/frontend/teacherExamReview.php
+++ rc/frontend/teacherExamReview.php
@@ -58,30 +58,30 @@
 					question["function"]["itemId"] = qtable.rows[0].cells[0].id;
 					question["colon"]["itemId"] = qtable.rows[0].cells[1].id;
 					question["constraints"]["itemId"] = qtable.rows[0].cells[2].id;
-					if (qtable.rows[0].cells[0].firstChild.firstChild.value == "") {
-						question["function"]["pointsEarned"] = qtable.rows[i].cells[0].firstChild.firstChild.placeholder;
+					if (qtable.rows[0].cells[0].firstChild.value == "") {
+						question["function"]["pointsEarned"] = qtable.rows[i].cells[0].firstChild.placeholder;
 					}
 					else {
-						question["function"]["pointsEarned"] = qtable.rows[i].cells[0].firstChild.firstChild.value;
+						question["function"]["pointsEarned"] = qtable.rows[i].cells[0].firstChild.value;
 					}
-					if (qtable.rows[0].cells[1].firstChild.firstChild.value == "") {
-						question["colon"]["itemId"]["pointsEarned"] = qtable.rows[i].cells[1].firstChild.firstChild.placeholder;
-					}
-					else {
-						question["colon"]["itemId"]["pointsEarned"] = qtable.rows[i].cells[1].firstChild.firstChild.value;
-					}
-					if (qtable.rows[0].cells[2].firstChild.firstChild.value == "") {
-						question["constraints"]["pointsEarned"] = qtable.rows[i].cells[2].firstChild.firstChild.placeholder;
+					if (qtable.rows[0].cells[1].firstChild.value == "") {
+						question["colon"]["itemId"]["pointsEarned"] = qtable.rows[i].cells[1].firstChild.placeholder;
 					}
 					else {
-						question["constraints"]["pointsEarned"] = table.rows[i].cells[2].firstChild.firstChild.value;
+						question["colon"]["itemId"]["pointsEarned"] = qtable.rows[i].cells[1].firstChild.value;
+					}
+					if (qtable.rows[0].cells[2].firstChild.value == "") {
+						question["constraints"]["pointsEarned"] = qtable.rows[i].cells[2].firstChild.placeholder;
+					}
+					else {
+						question["constraints"]["pointsEarned"] = table.rows[i].cells[2].firstChild.value;
 					}
 					question["testCases"] = {};
 					var tTable = document.getElementById("".concat(questionId, "testCases"));
 					for (var i=1; i<tTable.rows.length-1; i++) {
 						let temp = {};
 						temp["itemId"] = tTable.rows[i].id;
-						temp["pointsEarned"] = tTable.rows[i].cells[1].firstChild.firstChild.value
+						temp["pointsEarned"] = tTable.rows[i].cells[1].firstChild.value
 						questions["testCases"].push(temp);
 					}
 					question['questionId'] = questionId;
@@ -139,7 +139,7 @@
 							echo '<tr><th>Function Name</th><th>Colon</th><th>Constraint</th><th>Test Cases</th></tr>';
 							echo '<tr><td id="'.$json[$i]["function"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["function"]["pointsEarned"]."'>"." /".$json[$i]["function"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["function"]["pointsEarned"];
-							echo '<td id="'.$json[$i]["colon"]["itemId"].'">'."<input style='width: 100%;' placeholder='".$json[$i]["colon"]["pointsEarned"]."'>"." /".$json[$i]["colon"]["totalSubPoints"]."</td>";
+							echo '<td id="'.$json[$i]["colon"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["colon"]["pointsEarned"]."'>"." /".$json[$i]["colon"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["colon"]["pointsEarned"];
 							echo '<td id="'.$json[$i]["constraints"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["constraints"]["pointsEarned"]."'>"." /".$json[$i]["constraints"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["constraints"]["pointsEarned"];
