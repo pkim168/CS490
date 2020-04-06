7990519e775d14ec00d8384e914144ac0b1ea9c8
dpn418 <60945105+dpn418@users.noreply.github.com>
2020-04-05 20:54:24 -0400

    Update teacherExamReview.php

--- rc/frontend/teacherExamReview.php
+++ rc/frontend/teacherExamReview.php
@@ -108,11 +108,11 @@
 							echo "<td>".$json[$i]["answer"]."</td>";
 							echo '<td><table id="'.$json[$i]["questionId"].'points" style="width:100%">';
 							echo '<tr><th>Function Name</th><th>Colon</th><th>Constraint</th><th>Test Cases</th></tr>';
-							echo '<tr><td id="'.$json[$i]["function"]["itemId"].'">'."<input style='width: 50%;' placeholder='".$json[$i]["function"]["pointsEarned"]."'>"."/".$json[$i]["function"]["totalSubPoints"]."</td>";
+							echo '<tr><td id="'.$json[$i]["function"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["function"]["pointsEarned"]."'>"." /".$json[$i]["function"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["function"]["pointsEarned"];
-							echo '<td id="'.$json[$i]["colon"]["itemId"].'">'."<input style='width: 50%;' placeholder='".$json[$i]["colon"]["pointsEarned"]."'>"."/".$json[$i]["colon"]["totalSubPoints"]."</td>";
+							echo '<td id="'.$json[$i]["colon"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["colon"]["pointsEarned"]."'>"." /".$json[$i]["colon"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["colon"]["pointsEarned"];
-							echo '<td id="'.$json[$i]["constraints"]["itemId"].'">'."<input style='width: 50%;' placeholder='".$json[$i]["constraints"]["pointsEarned"]."'>"."/".$json[$i]["constraints"]["totalSubPoints"]."</td>";
+							echo '<td id="'.$json[$i]["constraints"]["itemId"].'">'."<input style='width: 25%;' placeholder='".$json[$i]["constraints"]["pointsEarned"]."'>"." /".$json[$i]["constraints"]["totalSubPoints"]."</td>";
 							$totalPointsEarned += $json[$i]["constraints"]["pointsEarned"];
 							echo '<td><table id="'.$json[$i]["questionId"].'testCases" style="width:100%">';
 							$testCases = $json[$i]["testCases"];
@@ -125,7 +125,7 @@
 								}
 								$str .= $parameters."\nOutput: ".$data['result'];
 								echo "<td><pre>".$str."</pre></td>";
-								echo "<td>"."<input style='width: 50%;' placeholder='".$testCases[$j]["pointsEarned"]."'>"."/".$testCases[$j]["totalSubPoints"]."</td></tr>";
+								echo "<td>"."<input style='width: 25%;' placeholder='".$testCases[$j]["pointsEarned"]."'>"." /".$testCases[$j]["totalSubPoints"]."</td></tr>";
 								$totalPointsEarned += $testCases[$j]["pointsEarned"];
 							}
 							echo "</table></td></tr></table></td>";						
