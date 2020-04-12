c2792452b9ae862314ea2b7b0171996cb8d25491
Jose Dominguez <41589772+jose-dom@users.noreply.github.com>
2020-04-12 00:14:27 -0400

    update
    update

--- rc/middleend/grading.php
+++ rc/middleend/grading.php
@@ -137,7 +137,7 @@ function grade($answer, $questionId, $functionName, $backend_constraints, $backe
         }
         $parameters = substr($parameters, 0, -1);
         //inserting code into file
-        file_put_contents($file,  $student_answer . "\n" . "print($answer_function_name($parameters))");
+        file_put_contents($file, "#!/usr/bin/env python" . $student_answer . "\n" . "$answer_function_name($parameters)");
         //running the python code
         $runpython = exec("python $file");
         //checking if code matches the result
