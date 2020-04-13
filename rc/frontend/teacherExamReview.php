f2b5402bfbbf1e5c5b64b54f7f095be8a4caa500
Paul Kim <pkim168@gmail.com>
2020-04-12 21:37:54 -0400

    Update teacherExamReview.php

--- rc/frontend/teacherExamReview.php
+++ rc/frontend/teacherExamReview.php
@@ -101,6 +101,7 @@
 					console.log("one loop done");
 				}
 				console.log(formData);
+				return false;
 				// cURL to middle end
 				fetch("https://web.njit.edu/~jrd62/CS490/rc/teacher_middle_exam.php", {
 					method: "POST",
